e05b7294976bc57965762157aa0508d35569564e
Sun Howwrongbum <sun@libermatic.com>
2020-03-06 22:45:38 +0530

    fix: update idb schema

--- posw/public/js/store/db.js
+++ posw/public/js/store/db.js
@@ -1,8 +1,14 @@
 import Dexie from 'dexie';
+import relationships from 'dexie-relationships';
 
-const db = new Dexie('posw');
+const db = new Dexie('posw', { addons: [relationships] });
 db.version(1).stores({
-  party_details: '++id, party_type, party, pos_profile, updatedAt',
+  customers:
+    'name, customer_name, customer_group, territory, mobile_no, primary_address, modified',
+  items: 'name, item_name, description, item_group, customer_code, modified',
+  item_barcodes: 'name, barcode, parent -> items.name, modified',
+  uom_conversion_details:
+    'name, uom, conversion_factor, parent -> items.name, modified',
 });
 
 export default db;
