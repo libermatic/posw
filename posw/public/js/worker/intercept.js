72947c077812a05c2d7e4b21609dd20d99dddce8
Sun Howwrongbum <sun@libermatic.com>
2020-03-19 23:53:48 +0530

    refactor: setup basic logger

--- posw/public/js/worker/intercept.js
+++ posw/public/js/worker/intercept.js
@@ -2,21 +2,32 @@ import * as methods from './methods';
 import { getMethod, getParams, getArgs, respond } from './utils';
 
 export async function request(_request) {
+  const method = getMethod(_request);
+  const payload = await getPayload(_request);
+  if (payload) {
+    return respond(payload);
+  }
+  method && console.log(method);
+  return null;
+}
+
+async function getPayload(_request) {
   const req = _request.clone();
   const method = getMethod(req);
   if (method === 'erpnext.selling.page.point_of_sale.point_of_sale.get_items') {
     const args = getArgs(await req.text());
     const message = await methods.get_items(args);
-    return message && respond({ message });
+    return message && { message };
   }
   if (method === 'frappe.desk.search.search_link') {
     const args = getArgs(await req.text());
     const results = await methods.search_link(args);
-    return results && respond({ results });
+    return results && { results };
   }
   if (method === 'frappe.desk.form.utils.validate_link') {
     const args = getParams(req);
     const valid = await methods.validate_link(args);
-    return valid && respond(valid);
+    return valid;
   }
+  return null;
 }
