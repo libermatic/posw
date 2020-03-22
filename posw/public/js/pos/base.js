cb93359597d45fb29092cdf37432aed1d5eff33f
Sun Howwrongbum <sun@libermatic.com>
2020-03-22 20:43:06 +0530

    refactor: setup extension factory

--- posw/public/js/pos/base.js
+++ posw/public/js/pos/base.js
@@ -1,33 +1,30 @@
 import { putSetting } from '../store';
+import makeExtension from './factory';
 
 export default function base(Pos) {
-  if (Pos.extensions && Pos.extensions.includes('base')) {
-    return Pos;
-  }
-
-  class PosWithBase extends Pos {
-    constructor(wrapper) {
-      super(wrapper);
-      this.load_master_data();
-    }
-    async load_master_data() {
-      const registration = await navigator.serviceWorker.ready;
-      if (registration.active) {
-        registration.active.postMessage({
-          action: 'load_master_data',
-          payload: { csrf_token: frappe.csrf_token },
-        });
+  return makeExtension(
+    'base',
+    class PosWithBase extends Pos {
+      constructor(wrapper) {
+        super(wrapper);
+        this.load_master_data();
+      }
+      async load_master_data() {
+        const registration = await navigator.serviceWorker.ready;
+        if (registration.active) {
+          registration.active.postMessage({
+            action: 'load_master_data',
+            payload: { csrf_token: frappe.csrf_token },
+          });
+        }
+      }
+      async set_pos_profile_data() {
+        const resolved = await super.set_pos_profile_data();
+        if (this.frm.doc.pos_profile) {
+          putSetting('pos_profile', this.frm.doc.pos_profile);
+        }
+        return resolved;
       }
     }
-    async set_pos_profile_data() {
-      const resolved = await super.set_pos_profile_data();
-      if (this.frm.doc.pos_profile) {
-        putSetting('pos_profile', this.frm.doc.pos_profile);
-      }
-      return resolved;
-    }
-  }
-  const extensions = PosWithBase.extensions || [];
-  PosWithBase.extensions = [...extensions, 'base'];
-  return PosWithBase;
+  );
 }
--- posw/public/js/pos/stats.js
+++ posw/public/js/pos/stats.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue.js';
 
 import Stats from '../vue/Stats.vue';
+import makeExtension from './factory';
 
 function showStats() {
   const dialog = new frappe.ui.Dialog({
@@ -17,20 +18,16 @@ function showStats() {
 }
 
 export default function base(Pos) {
-  if (Pos.extensions && Pos.extensions.includes('stats')) {
-    return Pos;
-  }
-
-  class PosWithStats extends Pos {
-    constructor(wrapper) {
-      super(wrapper);
+  return makeExtension(
+    'stats',
+    class PosWithStats extends Pos {
+      constructor(wrapper) {
+        super(wrapper);
+      }
+      prepare_menu() {
+        super.prepare_menu();
+        this.page.add_menu_item(__('Stats'), showStats);
+      }
     }
-    prepare_menu() {
-      super.prepare_menu();
-      this.page.add_menu_item(__('Stats'), showStats);
-    }
-  }
-  const extensions = PosWithStats.extensions || [];
-  PosWithStats.extensions = [...extensions, 'base'];
-  return PosWithStats;
+  );
 }
