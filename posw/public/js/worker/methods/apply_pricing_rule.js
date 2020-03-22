0f47e1896197583b9e17bf3a5ae47beea505cc43
Sun Howwrongbum <sun@libermatic.com>
2020-03-22 17:26:55 +0530

    fix: make getRowWithPricingRule resuable

--- posw/public/js/worker/methods/apply_pricing_rule.js
+++ posw/public/js/worker/methods/apply_pricing_rule.js
@@ -1,15 +1,19 @@
 import * as R from 'ramda';
 
-// TODO: currently a stub. implement pricing rule
-export default async function({ args, doc = null }) {
-  const item = R.head(JSON.parse(args).items) || {};
+export function getRowWithPricingRule(row) {
   return {
-    doctype: item.doctype,
-    name: item.name,
-    parent: item.parent,
-    parenttype: item.parenttype,
-    child_docname: item.child_docname,
+    doctype: row.doctype,
+    name: row.name,
+    parent: row.parent,
+    parenttype: row.parenttype,
+    child_docname: row.child_docname,
     discount_percentage_on_rate: [],
     discount_amount_on_rate: [],
   };
+}
+
+// TODO: currently a stub. implement pricing rule
+export default async function({ args, doc = null }) {
+  const item = R.head(JSON.parse(args).items) || {};
+  return [getRowWithPricingRule(item)];
 }
